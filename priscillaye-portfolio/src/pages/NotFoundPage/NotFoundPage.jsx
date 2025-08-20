5282bf93ca4cdce64f9adcb1955da88039f5adb3
ScriptKitKat <priscilla.ye@gmail.com>
2025-08-19 22:17:45 -0500

    Not foundy

--- priscillaye-portfolio/src/pages/NotFoundPage/NotFoundPage.jsx
+++ priscillaye-portfolio/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,10 @@
+import "./NotFoundPage.scss";
 const NotFound = () => {
     return (
         <div className="not-found-page">
             <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
+            <p>Sorry, the page you are looking for does not exist.... try another page???? (p.s. there is a super secret hidden page but you can only access it via typing it on the url... ;)</p>
+            <Link to="/">Return to Home</Link>
         </div>
     );
 };
