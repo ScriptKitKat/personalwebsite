e7f82bf3cf96a34d2e41d57418153e7d438ec83c
ScriptKitKat <priscilla.ye@gmail.com>
2025-08-19 22:16:00 -0500

    my work updated

--- priscillaye-portfolio/src/components/information/WorkInfo.jsx
+++ priscillaye-portfolio/src/components/information/WorkInfo.jsx
@@ -4,29 +4,63 @@ import "./MyWork.scss";
 const MyWork = () => {
   const projectList = [
     {
-      title: "Aggie Nexus (Coming Soon)",
+      title: "Restoria",
       description:
-        "Online platform that connects students, alumni, and faculty to collaborate on entrepreneurial projects, share ideas, and access mentorship and funding.",
+        "A Video Game about helping the environment by saving birbs",
       logo: "/images/aggienexus.png",
-      links: [],
-    },
-    {
-      title: "nest.rec",
-      description:
-        "A security app that uses AI to recognize and log detected human figures. Compatible with Google Nest®.",
-      logo: "/images/nestrec.png",
-      links: [
-        { type: "github", url: "https://github.com/your-repo" },
+      tech: ["Godot", "Pixel Art", "Shaders"],
+      links: [{type: "github", url: "https://github.com/ScriptKitKat/game"},
+                {type: "demo", url: "https://scriptkitkat.itch.io/restoria"}
       ],
     },
     {
-      title: "basic.ify",
+      title: "CoralGuard",
       description:
-        "A web app that allows Spotify users to see how unique their music taste is.",
-      logo: "/images/spotify.png",
+        "A web app that helps protect coral reefs with through AI early bleaching dectection",
+      logo: "/images/nestrec.png",
+      tech: ["Flask", "Python", "Collab", "PyTorch"],
       links: [
-        { type: "github", url: "https://github.com/your-repo" },
-        { type: "website", url: "https://basicify.vercel.app" },
+        { type: "github", url: "https://github.com/ScriptKitKat/Coral" },
+        { type: "demo", url: "https://devpost.com/software/coralguard-52pxvk" },
+      ],
+    },
+    {
+      title: "Rapid Removal",
+      description:
+        "Website for a waste removal company with a modern, sleek style️",
+      logo: "/images/spotify.png",
+      tech: ["TypeScript", "Next", "Tailwind CSS", "Node.js", "Figma"],
+      links: [
+        { type: "github", url: "https://github.com/ScriptKitKat/rapid-removal" },
+        { type: "website", url: "https://rapid-removal.vercel.app" },
+      ],
+    },
+    {
+      title: "Extended Essay",
+      description:
+        "Essay on the mathematics behind Neural Networks",
+      logo: "/images/spotify.png",
+      tech: ["Latex", "Python", "Google Collab"],
+      links: [
+        { type: "essay", url: "https://github.com/ScriptKitKat/Extended-Essay" },
+      ],
+    },
+    {
+      title: "CURIEosity",
+      description: "Founder of a female-led non-profit that promotes STEM education",
+      logo: "/images/spotify.png",
+      tech: ["TypeScript", "Next", "Tailwind CSS", "Node.js"],
+      links: [
+        { type: "website", url: "https://www.curieosity.org/" },
+      ],
+    },
+    {
+      title: "Personal Portfolio",
+      description: "This project! lollll",
+      logo: "/images/spotify.png",
+      tech: ["3JS", "Blender", "Vite", "React", "GSAP"],
+      links: [
+        { type: "website", url: "https://priscillaye.me/" },
       ],
     },
   ];
@@ -41,6 +75,11 @@ return (
                     <div className="project-info">
                         <h3>{proj.title}</h3>
                         <p>{proj.description}</p>
+                        <ul className="project-tech">
+                            {proj.tech.map((t, idx) => (
+                            <li key={idx}>{t}</li>
+                            ))}
+                        </ul>
                         <div className="project-links">
                             {proj.links.map((link, j) => (
                                 <a
